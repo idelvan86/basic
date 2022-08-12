8acbf8fefde5613242afe4c4cad40cf42142ecd2
idelvan86 <idelvan.isidorio@hotmail.com>
2022-08-12 03:15:55 -0300

    Adição de telas
    adição das telas do painel admin

---
+++ database/migrations/2022_08_02_233017_create_sliders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->text('descricao')->nullable();
+            $table->string('imagem')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
