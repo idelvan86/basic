387858943b045ce5b8ad5c4b5d6a809aee5f27f3
idelvan86 <idelvan.isidorio@hotmail.com>
2022-10-18 20:55:00 -0300

    Update create.blade.php
    fazendo about page

--- resources/views/admin/sobre/create.blade.php
+++ resources/views/admin/sobre/create.blade.php
@@ -6,25 +6,27 @@
 <div class="col-lg-12">
     <div class="card card-default">
         <div class="card-header card-header-border-bottom">
-            <h2>Adicionar Slider</h2>
+            <h2>Adicionar Sobre</h2>
         </div>
         <div class="card-body">
         <form action="{{ route('salvar.sobre') }}" method="POST" enctype="multipart/form-data"> 
             @csrf
                 <div class="form-group">
-                    <label for="exampleFormControlInput1">Titulo Slider</label>
+                    <label for="exampleFormControlInput1">Titulo Sobre</label>
                     <input type="text" class="form-control" id="titulo" name="titulo" placeholder="informe o titulo" required>
                     
                 </div>
-            
+                
                 <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Descrição</label>
-                    <textarea class="form-control" id="textarea" name="descricao" rows="3"></textarea>
+                    <label for="exampleFormControlTextarea1">Texto Curto</label>
+                    <textarea class="form-control" id="textarea" name="texto_curto" rows="2" placeholder="Texto curto sobre"></textarea>
                 </div>
+
                 <div class="form-group">
-                    <label for="ile1">Enviar arquivo</label>
-                    <input type="file" class="form-control-file" name="imgslider" id="exampleFormControlFile1">
+                    <label for="exampleFormControlTextarea1">Texto Longo</label>
+                    <textarea class="form-control" id="textarea" name="texto_longo" rows="4" placeholder="Texto completo sobre"></textarea>
                 </div>
+               
                 <div class="form-footer pt-4 pt-5 mt-4 border-top">
                     <button type="submit" class="btn btn-primary btn-default">Submit</button>
                     <button type="submit" class="btn btn-secondary btn-default">Cancel</button>
